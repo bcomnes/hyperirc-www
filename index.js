5e83c1d18d4ca77e0ea304d4711f2e26cce31b43
Bret Comnes <bcomnes@gmail.com>
2017-10-14 15:16:27 -0700

    Update to latest hypercore and add build process

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 var hypercore = require('hypercore')
-var level = require('level-browserify')
 var swarm = require('webrtc-swarm')
 var signalhub = require('signalhub')
 var pump = require('pump')
+var ram = require('random-access-idb')('hyperirc')
 
 var key = window.location.hash.slice(1)
 
 var all = false
 var cnt = 0
-var db = level('hyperirc.db')
-var core = hypercore(db)
+var core = hypercore(function (filename) {
+  // filename will be one of: data, bitfield, tree, signatures, key, secret_key
+  // the data file will contain all your data concattenated.
 
-var feed = core.createFeed(key, {sparse: true})
+  // just store all files in ram by returning a random-access-memory instance
+  return ram(filename)
+}, key, {
+  sparse: true
+})
+
 var $main = document.getElementById('main')
 
 function log (msg) {
@@ -20,45 +26,46 @@ function log (msg) {
   $main.appendChild(pre)
 }
 
-feed.get(0, function (err, channel) {
-  if (err) throw err
+core.on('ready', function () {
+  core.get(0, function (err, channel) {
+    if (err) throw err
 
-  document.title = document.getElementById('channel').innerText = channel.toString()
+    document.title = document.getElementById('channel').innerText = channel.toString()
 
-  var end = feed.blocks
+    var end = core.length
+    var stream = tail()
 
-  if (!all) {
-    feed.get(feed.blocks, function () {
-      if (feed.blocks - end > 25) {
-        stream.destroy()
-        log('(skipping to latest messages)')
-        tail()
-      }
-    })
-  }
+    if (!all) {
+      core.get(core.length, function () {
+        if (core.length - end > 25) {
+          stream.destroy()
+          log('(skipping to latest messages)')
+          tail()
+        }
+      })
+    }
 
-  var stream = tail()
-
-  function tail () {
-    var stream = feed.createReadStream({live: true, start: all ? 0 : Math.max(feed.blocks - 25, 1)})
+    function tail () {
+      var stream = core.createReadStream({live: true, start: all ? 0 : Math.max(core.blocks - 25, 1)})
       .on('data', function (data) {
         log(data.toString())
       })
 
-    return stream
-  }
-})
+      return stream
+    }
+  })
 
-var sw = swarm(signalhub('hyperirc-' + feed.discoveryKey.toString('hex'), 'https://signalhub.mafintosh.com'))
+  var sw = swarm(signalhub('hyperirc-' + core.discoveryKey.toString('hex'), 'https://signalhub.mafintosh.com'))
 
-console.log('Waiting for peers...')
+  console.log('Waiting for peers...')
 
-sw.on('peer', function (connection) {
-  console.log('(webrtc peer joined, %d total', ++cnt)
-  document.getElementById('count').innerText = '' + cnt
-  var peer = feed.replicate()
-  pump(peer, connection, peer, function () {
-    console.log('(webrtc peer left, %d total', --cnt)
+  sw.on('peer', function (connection) {
+    console.log('(webrtc peer joined, %d total', ++cnt)
     document.getElementById('count').innerText = '' + cnt
+    var peer = core.replicate()
+    pump(peer, connection, peer, function () {
+      console.log('(webrtc peer left, %d total', --cnt)
+      document.getElementById('count').innerText = '' + cnt
+    })
   })
 })
